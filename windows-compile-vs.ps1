88e0cf89ff038b05de1f845dba5a7c871eac2698
Dylan K. Taylor <dktapps@pmmp.io>
2025-09-12 00:07:52 +0100

    Bring back PHP 8.1 builds
    Since we still technically support this in PM, we need to test it, and it seems not that difficult to do support for.
    I think we only dropped support because it was annoying to maintain so many branches, but with the new repo architecture that's no longer a problem.

--- windows-compile-vs.ps1
+++ windows-compile-vs.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference="Stop"
 $ProgressPreference="SilentlyContinue"
 
-$PHP_VERSIONS=@("8.2.29", "8.3.25", "8.4.12")
+$PHP_VERSIONS=@("8.1.33", "8.2.29", "8.3.25", "8.4.12")
 
 $PHP_SDK_VER="2.3.0"
 $ARCH="x64"
